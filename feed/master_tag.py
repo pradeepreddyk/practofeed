349038eea5a4e9850e90b28916b208f9c8c344f6
Pradeep <pradeep.katukooru@practo.com>
2015-09-12 11:38:43 +0530

    add master_type and thumbnail to tags

--- feed/master_tag.py
+++ feed/master_tag.py
@@ -3,15 +3,19 @@ from feed import db
 class MasterTag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
+    master_type = db.Column(db.String(80))
+    thumbnail = db.Column(db.String(256))
     parent_id = db.Column(db.Integer, db.ForeignKey('master_tag.id'))                  
     parent = db.relationship('MasterTag',                                               
        backref=db.backref('children', lazy='dynamic'),
        remote_side=id)
 
-    def __init__(self, name, parent=None):
+    def __init__(self, name, master_type, thumbnail, parent=None):
         self.name = name
         self.parent = parent
+        self.master_type = master_type
+        self.thumbnail = thumbnail
 
     def __repr__(self):
-        return '<User %r>' % self.name
+        return '<MasterTag %r>' % self.name
 
